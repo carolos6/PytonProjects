03ff8ba77599426cbf87a59a2105946077e87f81
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-20 13:55:43 -0500

    EjercicioUno

---
+++ EjercicioUno.py
@@ -0,0 +1,15 @@
+num1=float(input("Ingrese el numero: "))
+num2=float(input("Ingrese el numero:"))
+suma=num1+num2
+resta=num1-num2
+multiplicacion=num1*num2
+division=num1/num2
+divisionEntera=num1//num2
+residuo=num1%num2
+print("Suma:", suma)
+print(f"Resta: {resta}")
+print(f"Multiplicacion: {multiplicacion}")
+print(f"Division: {division}")
+print(f"DivisionEntera: {divisionEntera}")
+print(f"Multiplicacion: {multiplicacion}")
+print(f"Residuo: {residuo}")
