9e955eae26f7997c9e7fbec74893a83a9d6d2996
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-20 15:10:10 -0500

    EjercicioTres

---
+++ EjercicioTares.py
@@ -0,0 +1,10 @@
+numero=float(input("Ingrese el numero: "))
+if numero >0:
+    print("El numero es positivo")
+elif numero < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
+    
+    
+    
