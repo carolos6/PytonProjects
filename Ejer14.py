ceb5b23f8168ee4a6ece956dd906c395e04ad63e
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-21 16:27:00 -0500

    ejer14

---
+++ Ejer14.py
@@ -0,0 +1,6 @@
+while True:
+    numero  = int(input("Introduce un numero positivo entero: "))
+    if numero > 0:
+        break
+    print("El numero tiene que ser mayor que 0. Intente otra vez.")
+    print("Haz introducido el numero: " , numero)
