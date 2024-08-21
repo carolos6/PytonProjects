f357858851d1fcd397869f03a15c3bc59a1881f0
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-21 11:54:10 -0500

    Ejerc5.py

---
+++ Ejer5.py
@@ -0,0 +1,19 @@
+calificacion= float(input("Ingrese la calificacion"))
+if calificacion>=90:
+    print("A")
+    letra='A'
+elif calificacion>=80 and calificacion<90:
+    print("B")
+    letra='B'
+elif calificacion>=70 and calificacion<80:
+    print("C")
+    letras='C'
+elif calificacion>=60 and calificacion<70:
+    print("D")
+    letras='D'
+else:
+    print("F")
+    letras='F'
+    
+
+    
