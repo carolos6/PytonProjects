042cda43455e7b6c9900d827d754873e77383e24
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-21 16:05:43 -0500

    ejer12

---
+++ Ejer12.py
@@ -0,0 +1,13 @@
+edad_Cliente = int (input("Edad del cliente: "))
+
+if edad_Cliente<4:
+    print("El cliente accede gratis")
+elif edad_Cliente>=4 and edad_Cliente<=18:
+    print("El cliente paga $5")
+else:
+    print("El cliente paga $10")
+    
+
+    
+    
+    
