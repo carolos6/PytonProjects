6c341a604ff53a6c6e88d70f4f0051b2c13e58e7
carolos6 <carolos_kk_xx@hotmail.com>
2024-08-22 09:58:51 -0500

    Lista de Tipo Tuple

---
+++ Ejer22.py
@@ -0,0 +1,5 @@
+numeros=(1,2,3,4,5)
+resultado= tuple(num*2 for num in numeros)
+print("Nuevo resultado: ", resultado)
+
+#tuple contenido en parentesis recorres la tuple y multiplicas su contenido 
